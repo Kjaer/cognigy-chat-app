d2a6063882f3e371677925812300662de5afa187
Halil Kayer <halil.kayer@memsource.com>
2022-09-09 20:45:28 +0200

    feat: add auto scroll to down when there is new messages

--- src/components/history/History.tsx
+++ src/components/history/History.tsx
@@ -1,9 +1,17 @@
-import React, { FC, PropsWithChildren } from "react";
+import React, { FC, PropsWithChildren, useLayoutEffect, useRef } from "react";
 import styles from "./History.module.css";
 
 export const History: FC<PropsWithChildren> = (props) => {
+  const messageWrapper = useRef<HTMLElement>(null);
+
+  useLayoutEffect(() => {
+    if(messageWrapper.current){
+      messageWrapper.current.scrollTop = messageWrapper.current.scrollHeight;
+    }
+  })
+
   return (
-    <section className={styles.history}>
+    <section className={styles.history} ref={messageWrapper}>
       <div>{props.children}</div>
     </section>
   );
